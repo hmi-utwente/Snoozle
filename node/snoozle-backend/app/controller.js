5921060689f357857eeb087ae8b73b6350e0aac9
Jan Kolkmeier <jankolkmeier@gmail.com>
2017-02-28 00:32:11 +0100

    updated wizard

--- node/snoozle-backend/app/controller.js
+++ node/snoozle-backend/app/controller.js
@@ -3,7 +3,7 @@ window._stream = false;
 
 app.factory('stomp', function($rootScope) {
 	var stomp_location = "ws://127.0.0.1:61623";
-	var options = {debug: true, protocols: webstomp.VERSIONS.supportedProtocols()};//
+	var options = {debug: false, protocols: webstomp.VERSIONS.supportedProtocols()};//
 	if (location.hash.length > 1) {
 		stomp_location = location.hash.substring(1);
 	}
@@ -14,8 +14,7 @@ app.factory('stomp', function($rootScope) {
 		console.log("Connected to stomp.");
 		location.hash = "#"+stomp_location;
 		var subscriptionBML = client.subscribe('/topic/ASAPSnoozleBmlFeedback', function(msg) {
-			console.log("StudyState:");
-			console.log(msg);
+			console.log("BmlFeedback: "+msg);
 		});
 	}, function(c, e) {
 		console.log("Error connecting to stomp: "+e);
@@ -62,6 +61,14 @@ app.factory('socket', function($rootScope) {
 });
 
 function RemoteCTRL($scope, socket, stomp, ngAudio) {
+    $scope.defaultBml = '<bmlt:audiofile start="0" id="audio0" fileName="Snoozle/audio/Snooze/weeeeh.wav" />\n'+
+                        '<mw:sendJsonMessage start="0" id="serv0">{\n'+
+                        '  "msgType": "setServo",\n'+
+                        '  "servo": [1,2,3,4],\n'+
+                        '  "position": 20\n'+
+                        '  "stepDelay": 5,\n'+
+                        '  "stepSize": 1\n'+
+                        '}</mw:sendJsonMessage>'
     $scope.glued = true;
     $scope.cmddata = "";
     $scope.log = [];
@@ -76,53 +83,8 @@ function RemoteCTRL($scope, socket, stomp, ngAudio) {
 		"blocks": []
 	};
 
-	$scope.startExperiment = function() {
-		if (window.confirm("Unpaused PhysioRecording?")) {
-			if (window.confirm("Calibrated Eye-tracker?")) {
-				$scope.experimentRunning = true;
-				$scope.dialogEditMode = false;
-				$scope.experimentControl('START');
-				//$scope.send('START_EXPERIMENT');
-				angular.element('.pure-button').removeClass('playing');
-				angular.element('.pure-button').removeClass('completed');
-			}
-		}
-	}
-	
-	$scope.endExperiment = function() {
-		$scope.experimentRunning = false;
-		//angular.element('#timer').removeClass('running');
-		$scope.experimentControl('STOP');
-		//$scope.send('STOP_EXPERIMENT');
-		//window.confirm("Stop video recordings!.");
-	}
-	
-    $scope.send = function(raw) {
-		console.log("Send broken...")
-		
-    }
-
     $scope.resetPillow = function() {
-         var resetBml = '<mw:sendJsonMessage id="serv1" start="0">{'+
-                        '    "msgType": "setServo",'+
-                        '    "servo": 1,'+
-                        '    "position": 20'+
-                        '}</mw:sendJsonMessage>'+
-                        '<mw:sendJsonMessage id="serv2" start="0">{'+
-                        '    "msgType": "setServo",'+
-                        '    "servo": 2,'+
-                        '    "position": 20'+
-                        '}</mw:sendJsonMessage>'+
-                        '<mw:sendJsonMessage id="serv3" start="0">{'+
-                        '    "msgType": "setServo",'+
-                        '    "servo": 3,'+
-                        '    "position": 20'+
-                        '}</mw:sendJsonMessage>'+
-                        '<mw:sendJsonMessage id="serv4" start="0">{'+
-                        '    "msgType": "setServo",'+
-                        '    "servo": 4,'+
-                        '    "position": 20'+
-                        '}</mw:sendJsonMessage>';
+        var resetBml = '<mw:sendJsonMessage id="serv0" start="0">{ "msgType": "setServo", "position": 20 }</mw:sendJsonMessage>';
         $scope.pillowBML(resetBml);
     }
 	
@@ -135,15 +97,11 @@ function RemoteCTRL($scope, socket, stomp, ngAudio) {
 			  content: encodeURIComponent((prefix+text+suffix))
 		  }
         };
-		stomp.send('/topic/ASAPSnoozleBmlRequest', JSON.stringify(msg));
-	}
-	
-	$scope.experimentControl = function(cmd) {
-		var msg = {
-          type:"cmd",
-          data: { cmd: cmd }
-        };
-        socket.send(JSON.stringify(msg));
+        try {
+		    stomp.send('/topic/ASAPSnoozleBmlRequest', JSON.stringify(msg));
+        } catch(err) {
+            console.log("Websocket? ", err);
+        }
 	}
 	
 	$scope.randomID = function() {
@@ -151,10 +109,6 @@ function RemoteCTRL($scope, socket, stomp, ngAudio) {
 		   return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
 		};
 		return (S4()+'_'+S4());
-	}
-	
-	$scope.updateIDs = function() {
-		
 	}
 	
 	$scope.toggleEditMode = function() {
@@ -197,55 +151,14 @@ function RemoteCTRL($scope, socket, stomp, ngAudio) {
 
     socket.on('message', function(data){
       var msg = JSON.parse(data);
-      if (msg.type == "refresh") {
-		  /*
-        $scope.f_agentSpeech = [];
-		$scope.agentClips = {};
-        for (var i = 0; i < msg.agentSpeech.length; i++) {
-          if (msg.agentSpeech[i].indexOf(".ogg") > 1 && msg.agentSpeech[i].indexOf(".meta") < 0) {
-            $scope.f_agentSpeech.push(msg.agentSpeech[i].split('.')[0]);
-			$scope.agentClips[msg.agentSpeech[i].split('.')[0]] = ngAudio.load(msg.agentSpeech[i]);
-          }
-        }*/
-      } else if (msg.type == "res") {
-		  if (msg.data[0] == 'c') {
-			var cmd = msg.data.substring(2).split(" ");
-			$scope.experimentState = cmd[0];
-			minutes = parseInt(cmd[1] / 60, 10);
-			seconds = parseInt(cmd[1] % 60, 10);
-			minutes = minutes < 10 ? "0" + minutes : minutes;
-			seconds = seconds < 10 ? "0" + seconds : seconds;
-			$scope.timer = minutes + ":" + seconds;
-
-			  /*
-			if (cmd[0] == "DIST") {
-				$scope.l_distance = parseFloat(cmd[1]);
-			} else if (cmd[0] == "SAYING") {
-				if ($scope.agentClips[cmd[1]]) {
-					$scope.agentClips[cmd[1]].play();
-					console.log("Playing: "+cmd[1]);
-				} else {
-					console.log("Don't know audio clip for "+cmd[1]);
-				}
-				
-				angular.element('.pure-button').removeClass('playing');
-				angular.element('#'+cmd[1]).addClass('playing');
-				angular.element('#'+cmd[1]).addClass('completed');
-			} else if (cmd[0] == 'STOPSAYING') {
-				angular.element('.pure-button').removeClass('playing');
-			}*/
-		  } else {
-			var cls = "log_"+msg.data[0];
-			$scope.log.push({ class:cls, txt:msg.data.substring(2) });
-		  }
-		  
-      } else if (msg.type == "dialog") {
+      if (msg.type == "dialog") {
 		  $scope.dialogJson = msg.dialog;
 	  }
     });
 
     socket.on('close', function(){
     });
+
     socket.on('open', function(){
       $scope.refresh();
 	  $scope.loadDialog();
